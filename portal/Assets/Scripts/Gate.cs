36beacfb8b6d23601ee0aaa62ac94585187ca3b1
boraozenbirkan <59583105+boraozenbirkan@users.noreply.github.com>
2021-03-22 17:11:51 +0300

    Portal - Finished
    - New game level added
    - "You Won" text added

---
+++ portal/Assets/Scripts/Gate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour{
+
+    [SerializeField] Canvas canvas = null;
+
+    private void OnTriggerEnter(Collider col){
+        canvas.enabled = true;
+        Time.timeScale = 0;
+    }
+    
+}
