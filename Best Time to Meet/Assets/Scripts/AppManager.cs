d82f1d6e5fd75716af86fb8a6f481e214f3f4eed
boraozenbirkan <59583105+boraozenbirkan@users.noreply.github.com>
2021-04-01 13:05:45 +0300

    BTM Project Initial Setup
    Changes:
    - Created BTM project again.
    - Added login and register pages

---
+++ Best Time to Meet/Assets/Scripts/AppManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AppManager : MonoBehaviour{
+
+    public static AppManager instance;
+
+    [Header ("Landing Scene Objects")]
+    [SerializeField] GameObject loginCanvas = null;
+    [SerializeField] GameObject registerCanvas = null;
+    [SerializeField] TextMeshProUGUI loginUsernameInput = null;
+    [SerializeField] TextMeshProUGUI loginPasswordInput = null;
+    [SerializeField] TextMeshProUGUI registerUsernameInput = null;
+    [SerializeField] TextMeshProUGUI registerPasswordInput = null;
+    [SerializeField] TextMeshProUGUI registerConfirmationInput = null;
+    [SerializeField] TextMeshProUGUI messageText = null;
+
+
+    void Awake(){
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+    }
+
+    #region Buttons
+
+    public void LandingScene(){
+        loginCanvas.SetActive(true);
+        registerCanvas.SetActive(false);
+        // Clear the inputs
+        loginUsernameInput.text = loginPasswordInput.text = "";
+    }
+
+    public void RegisterScene() {
+        loginCanvas.SetActive(false);
+        registerCanvas.SetActive(true);
+
+        // Clear the inputs
+        registerUsernameInput.text = registerPasswordInput.text = registerConfirmationInput.text = "";
+    }
+
+    #endregion
+
+}
