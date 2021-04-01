0234cb769ae0811fa5a7fa422a91f3d9131c5c0e
boraozenbirkan <59583105+boraozenbirkan@users.noreply.github.com>
2021-04-01 17:29:47 +0300

    BTM - Local Setup
    Changes:
    - We set up login/logout
    - Users can create new calendars

--- Best Time to Meet/Assets/Scripts/AppManager.cs
+++ Best Time to Meet/Assets/Scripts/AppManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class AppManager : MonoBehaviour{
 
@@ -17,12 +18,41 @@ public class AppManager : MonoBehaviour{
     [SerializeField] TextMeshProUGUI registerConfirmationInput = null;
     [SerializeField] TextMeshProUGUI messageText = null;
 
+    [Header("Plan Scene Objects")]
+    [SerializeField] GameObject planLandingCanvas = null;
+    [SerializeField] GameObject planCalendarCanvas = null;
+    [SerializeField] TextMeshProUGUI calIDText = null;
+    [SerializeField] TextMeshProUGUI planMessageText = null;
 
+    /*
     void Awake(){
         if (instance == null)
             instance = this;
         else
             Destroy(this);
+    }*/
+
+    public void SetCalanderID(string calID) {
+
+    }
+
+    public void CreateNewCalender() {
+        // Creat ID like 123-456
+        int calID = Random.Range(0, 1000);
+        calIDText.text = calID.ToString() + "-";
+        calID = Random.Range(0, 1000);
+        calIDText.text += calID.ToString();
+
+        planLandingCanvas.SetActive(false);
+        planCalendarCanvas.SetActive(true);
+
+        StartCoroutine(CallMessage(planMessageText, "New Calendar has been created!", 3f));
+    }
+
+    IEnumerator CallMessage(TextMeshProUGUI textBox, string msg, float seconds) {
+        textBox.text = msg;
+        yield return new WaitForSeconds(seconds);
+        textBox.text = "";
     }
 
     #region Buttons
@@ -31,7 +61,7 @@ public class AppManager : MonoBehaviour{
         loginCanvas.SetActive(true);
         registerCanvas.SetActive(false);
         // Clear the inputs
-        loginUsernameInput.text = loginPasswordInput.text = "";
+        loginUsernameInput.text = loginPasswordInput.text = messageText.text = "";
     }
 
     public void RegisterScene() {
@@ -39,7 +69,12 @@ public class AppManager : MonoBehaviour{
         registerCanvas.SetActive(true);
 
         // Clear the inputs
-        registerUsernameInput.text = registerPasswordInput.text = registerConfirmationInput.text = "";
+        registerUsernameInput.text = registerPasswordInput.text = 
+            registerConfirmationInput.text = messageText.text = "";
+    }
+
+    public void LogoutButton() {
+        SceneManager.LoadScene("LandingScene");
     }
 
     #endregion
